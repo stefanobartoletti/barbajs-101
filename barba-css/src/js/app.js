9a2a080cf7cb8d675c067cd79e21e4b34548970b
Stefano Bartoletti <bartoletti.stefano@gmail.com>
2022-07-25 12:40:14 +0200

    lesson 2

--- barba-css/src/js/app.js
+++ barba-css/src/js/app.js
@@ -1,1 +1,25 @@
+import barba from '@barba/core';
+import barbaCss from '@barba/css';
 
+// tell Barba to use the css plugin
+barba.use(barbaCss);
+
+// init Barba
+barba.init({
+  transitions: [
+    {
+      name: 'home',
+      beforeOnce() {
+        console.log('beforeOnce');
+      },
+      once() {
+
+        // When using CSS plugin, this is not run
+        console.log('once');
+      },
+      afterOnce() {
+        console.log('afterOnce');
+      },
+    },
+  ],
+});
