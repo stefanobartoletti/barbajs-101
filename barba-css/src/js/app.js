70239928b31d118c4ff37d3b7767614a3bd0a819
Stefano Bartoletti <bartoletti.stefano@gmail.com>
2022-07-25 17:41:54 +0200

    lesson 4

--- barba-css/src/js/app.js
+++ barba-css/src/js/app.js
@@ -4,6 +4,14 @@ import barbaCss from '@barba/css';
 // tell Barba to use the css plugin
 barba.use(barbaCss);
 
+const body = document.querySelector('body');
+
+barba.hooks.before((data) => {
+  const background = data.current.container.dataset.background;
+
+  body.style.setProperty('--page-background', background);
+});
+
 // init Barba
 barba.init({
   transitions: [
