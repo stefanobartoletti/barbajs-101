b222e6b82f2b34952d6742d317f0f2a9e4ce064b
Stefano Bartoletti <bartoletti.stefano@gmail.com>
2022-07-26 15:30:12 +0200

    lesson 9

--- barba-gsap/src/js/app.js
+++ barba-gsap/src/js/app.js
@@ -1,3 +1,34 @@
 import barba from '@barba/core';
 import gsap from 'gsap';
 
+const animationEnter = (container) => {
+  return gsap.from(container, {
+    autoAlpha: 0,
+    duration: 2,
+    clearProps: 'all',
+    ease: 'none',
+  });
+};
+
+const animationLeave = (container) => {
+  return gsap.to(container, {
+    autoAlpha: 0,
+    duration: 2,
+    clearProps: 'all',
+    ease: 'none',
+  });
+};
+
+barba.init({
+  transitions: [
+    {
+      once({ next }) {
+        animationEnter(next.container);
+      },
+      leave: ({ current }) => animationLeave(current.container),
+      enter({ next }) {
+        animationEnter(next.container);
+      },
+    },
+  ],
+});
