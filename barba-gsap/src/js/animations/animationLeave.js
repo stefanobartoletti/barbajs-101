df51ebc0d93891fb99ea34043a39702217e633e8
Stefano Bartoletti <bartoletti.stefano@gmail.com>
2022-07-26 16:35:55 +0200

    lesson 12

--- barba-gsap/src/js/animations/animationLeave.js
+++ barba-gsap/src/js/animations/animationLeave.js
@@ -1,12 +1,34 @@
 import gsap from 'gsap';
 
 const animationLeave = (container) => {
-  return gsap.to(container, {
-    autoAlpha: 0,
-    duration: 2,
-    clearProps: 'all',
-    ease: 'none',
+
+  const activeLink = container.querySelector('a.is-active span');
+  const images = container.querySelectorAll('.image');
+  const img = container.querySelectorAll('img');
+
+  const tl = gsap.timeline({
+    defaults: {
+      duration: 0.4,
+      ease: 'power1.in',
+    },
   });
+
+  tl
+    .to(activeLink, {
+      xPercent: 101,
+    }, 0)
+
+    .to(images, {
+      xPercent: 101,
+      stagger: 0.1,
+    }, 0)
+
+    .to(img, {
+      xPercent: -101,
+      stagger: 0.1,
+    }, 0);
+
+  return tl;
 };
 
 export default animationLeave;
