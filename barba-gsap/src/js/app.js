904ac460d1a4195a6f7c3210f835eb5f4e65faa9
Stefano Bartoletti <bartoletti.stefano@gmail.com>
2022-07-27 14:43:28 +0200

    lesson 18

--- barba-gsap/src/js/app.js
+++ barba-gsap/src/js/app.js
@@ -1,8 +1,48 @@
 import barba from '@barba/core';
 import barbaPrefetch from '@barba/prefetch';
+import barbaRouter from '@barba/router';
 import gsap from 'gsap';
 
 import { revealProject, leaveToProject, leaveFromProject, animationEnter, animationLeave } from './animations';
+
+const myRoutes = [
+  {
+    name: 'home',
+    path: '/index.html',
+  },
+  {
+    name: 'architecture',
+    path: '/architecture.html',
+  },
+  {
+    name: 'project-1',
+    path: '/detail-page-1.html',
+  },
+  {
+    name: 'project-2',
+    path: '/detail-page-2.html',
+  },
+  {
+    name: 'project-3',
+    path: '/detail-page-3.html',
+  },
+  {
+    name: 'project-4',
+    path: '/detail-page-4.html',
+  },
+  {
+    name: 'project-5',
+    path: '/detail-page-5.html',
+  },
+  {
+    name: 'project-6',
+    path: '/detail-page-6.html',
+  },
+];
+
+barba.use(barbaRouter, {
+  routes: myRoutes,
+});
 
 barba.use(barbaPrefetch);
 
@@ -11,8 +51,9 @@ const resetActiveLink = () => gsap.set('a.is-active span', {
   transformOrigin: 'left',
 });
 
-barba.hooks.enter(() => {
+barba.hooks.enter((data) => {
   window.scrollTo(0, 0);
+  console.log(data);
 });
 
 barba.init({
